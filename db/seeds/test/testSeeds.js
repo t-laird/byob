44eae39eeb4a79043e6237c5a55a0113ac687274
Thomas Laird <t6r6l5@gmail.com>
2018-02-07 23:38:38 -0700

    test seed structure - untested

--- db/seeds/test/testSeeds.js
+++ db/seeds/test/testSeeds.js
@@ -1,13 +1,39 @@
+/*
+const mockShapes = require('../../sdljf');
+const mockLocations = require('../../sdljf');
+const mockSightings = require('../../sdljf');
+*/
+
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+  return knex('sightings').del()
+    .then(() => {
+      return knex('shapes').del();
+    })
+    .then(() => {
+      return knex('locations').del();
+    })
+    .then(() => {
+      return Promise.all([
+        knex('shapes').insert(mockShapes).returning('id'),
+        knex('locations').insert(mockLocations).returning('id')
       ]);
+    })
+    .then(validIds => {
+      return knex('sightings').insert(
+        mockSightings.map( sighting => 
+          ({
+            location_id: validIds[0][0],
+            shape_id: validIds[1][0],
+            duration: sighting.duration,
+            summary: sighting.summary,
+            reported_time: sighting.date
+          })
+        )
+      )
+    })
+    .then(() => {
+      console.log('Seeded test data.');
     });
 };
